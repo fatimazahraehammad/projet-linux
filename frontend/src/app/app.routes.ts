5c81e323c6a909610d16ff24572e3767c3c48f82
Sihame <ennassiri.sihame@etu.uae.ac.ma>
2026-04-26 17:12:42 +0200

    feat: page home terminée

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { CollectionsComponent } from './pages/collections/collections';
+import { ProduitComponent } from './pages/produit/produit';
+import { PanierComponent } from './pages/panier/panier';
+import { CommandeComponent } from './pages/commande/commande';
+import { AProposComponent } from './pages/a-propos/a-propos';
+import { ContactComponent } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'collections', component: CollectionsComponent },
+  { path: 'produit/:id', component: ProduitComponent },
+  { path: 'panier', component: PanierComponent },
+  { path: 'commande', component: CommandeComponent },
+  { path: 'a-propos', component: AProposComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ frontend/src/app/pages/a-propos/a-propos.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-a-propos',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './a-propos.html',
+  styleUrl: './a-propos.css',
+})
+export class AProposComponent {}
