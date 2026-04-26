b47b796e40b5ba3d00b619efb39eec0c8cfdc82b
Sihame <ennassiri.sihame@etu.uae.ac.ma>
2026-04-26 17:12:42 +0200

    feat: page home terminée

---
+++ frontend/src/app/pages/commande/commande.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-commande',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './commande.html',
+  styleUrl: './commande.css',
+})
+export class CommandeComponent {}
---
+++ frontend/src/app/pages/panier/panier.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-panier',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './panier.html',
+  styleUrl: './panier.css',
+})
+export class PanierComponent {}
