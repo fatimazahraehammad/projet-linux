3fef2fab59b5d0ef639d4763667f969db7a8e080
fatimazahraehammad <hammad.fatimazahrae@etu.uae.ac.ma>
2026-05-15 17:05:15 +0100

    Add lifecycle blocks to resources in main.tf
    Added lifecycle blocks to ignore changes for name and public_key.

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,39 +21,43 @@ resource "aws_security_group" "projet_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 4200
     to_port     = 4200
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    ignore_changes = [name]
+  }
 }
 
 resource "aws_key_pair" "projet_key" {
   key_name   = "projet-linux-key"
   public_key = var.public_key
+
+  lifecycle {
+    ignore_changes = [public_key]
+  }
 }
 
 resource "aws_instance" "projet_server" {
