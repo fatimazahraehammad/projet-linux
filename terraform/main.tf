58163944f64d5ebbb6852fe8b8c17a77816b6199
fatimazahraehammad <hammad.fatimazahrae@etu.uae.ac.ma>
2026-05-19 03:12:31 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,70 +6,64 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region = var.aws_region
 }
-
 resource "aws_security_group" "projet_sg" {
   name        = "projet-linux-sg"
   description = "Security group pour projet linux"
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 4200
     to_port     = 4200
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   lifecycle {
     ignore_changes = [name]
   }
 }
-
 resource "aws_key_pair" "projet_key" {
   key_name   = "projet-linux-key"
   public_key = var.public_key
-
   lifecycle {
     ignore_changes = [public_key]
   }
 }
-
 resource "aws_instance" "projet_server" {
   ami                    = "ami-0989fb15ce71ba39e"
   instance_type          = "t3.micro"
   key_name               = aws_key_pair.projet_key.key_name
   vpc_security_group_ids = [aws_security_group.projet_sg.id]
-
   user_data = <<-EOF
     #!/bin/bash
     apt-get update
@@ -78,12 +72,10 @@ resource "aws_instance" "projet_server" {
     systemctl enable docker
     usermod -aG docker ubuntu
   EOF
-
   tags = {
     Name = "projet-linux-server"
   }
 }
-
 resource "aws_eip_association" "projet_eip" {
   instance_id   = aws_instance.projet_server.id
   allocation_id = "eipalloc-0a45a89a6ad713bf5"
